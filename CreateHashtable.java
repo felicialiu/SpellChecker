d36b36f5958c2b435bfb9c6a39c0dcb9a3b1f853
JonasHans <Jonas.vanoenen@gmail.com>
2014-02-13 15:26:06 +0100

    create hash ding

---
+++ CreateHashtable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+import java.long.*;
+
+
+public class CreateHashtable{
+	private BufferedReader wordLibrary;
+
+	private void hashLibrary(){
+		wordLibrary = new BufferedReader(new FileReader())
+	}
+}
---
+++ public class CreateHashtable{.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.long.*;
+
+public class CreateHashtable{
+	private BufferedReader wordLibrary;
+
+	private void hashLibrary(){
+		wordLibrary = new BufferedReader(new FileReader(british-english-insane.txt))
+	}
+}
