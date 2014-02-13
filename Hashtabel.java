9b0fe134ac81b5db11136af47cf89198acebf870
Felicia Liu <felicia@felicialiu.com>
2014-02-13 17:01:02 +0100

    donderdag commits

---
+++ Hashtabel.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.long.*;
+
+
+public class Hashtabel {
+	///// weet niet wat dit doet volgens mij is het niet nodig
+	private BufferedReader wordLibrary;
+
+	// Hashtabel actual array // liever linked-list gebruiken
+	String[][] hashArray;
+
+	// Load factor of the hash table, automatically on 0.75 since it's a
+	// good load_factor
+	private float load_factor = 0.75;
+
+	// intitial size of the hash table
+	private int hash_size;
+
+	// Constructor that creates the hash table object
+	public Hashtabel<K,V>(int size) {
+		// wordLibrary = new BufferedReader(new FileReader(wordfile));
+		hash_size = size;
+		hashArray = new String[hash_size][2];
+	}
+
+	// Maps the specified key to the specified value in the hashtable
+	private void put(K key, V value) {
+		int index = calcIndex(key);
+		hashArray
+	}
+
+	// Returns the value to which the specified key is mapped, or null
+	// if the map contains no mapping for the key.
+	private V get(K key) {
+		return null;
+	}
+}
