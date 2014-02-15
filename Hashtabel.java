2a8cdc5858c01252b48614e9e3ba08415f6405d5
JonasHans <Jonas.vanoenen@gmail.com>
2014-02-15 12:09:42 +0100

    linked list added

--- Hashtabel.java
+++ Hashtabel.java
@@ -5,7 +5,7 @@ import java.long.*;
 
 public class Hashtabel {
 	// Hashtabel actual array // liever linked-list gebruiken
-	String[][] hashArray;
+	Linkedlisthashentry[][] hashArray;
 
 	// Load factor of the hash table, automatically on 0.75 since it's a
 	// good load_factor
@@ -15,16 +15,15 @@ public class Hashtabel {
 	private int hash_size;
 
 	// Constructor that creates the hash table object
-	public Hashtabel<K,V>(int size) {
-		// wordLibrary = new BufferedReader(new FileReader(wordfile));
+	public Hashtabel<K,Y>(int size) {
 		hash_size = size;
-		hashArray = new String[hash_size][2];
+		hashArray = new Linkedlisthashentry(K key, V value);
 	}
 
 	// Maps the specified key to the specified value in the hashtable
 	private void put(K key, V value) {
 		int index = calcIndex(key);
-		hashArray
+		//hashArray
 	}
 
 	// Returns the value to which the specified key is mapped, or null
