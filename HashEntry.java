7d6113764e4064e368844ccb5f8bfa1897123720
Felicia Liu <felicia@felicialiu.com>
2014-02-18 19:44:06 +0100

    hashtabel object done

---
+++ HashEntry.java
@@ -0,0 +1,17 @@
+public class HashEntry {
+      private String key;
+      private String value;
+ 
+      HashEntry(String key, String value) {
+            this.key = key;
+            this.value = value;
+      }     
+ 
+      public String getKey() {
+            return key;
+      }
+ 
+      public String getValue() {
+            return value;
+      }
+}
